532ddbb1622b184663ad346e069f2de7bc931954
b79637c5-739c-5a1b-bc4d-9052097d14d1 <ftdsdcitestuser01@9dhnu6dk.mailosaur.net>
2025-05-29 23:46:07 -0300

    Adding OOP VideoGame homework

---
+++ bootcamp_project/Module 8/More_OOP_Concepts/VideoGame_Store_OOP_EMira1.ts
@@ -0,0 +1,78 @@
+class VideoGame {
+
+    // Static attributes
+    static availableGames: VideoGame[] = [];
+    static soldOutGames: VideoGame[] = [];
+    static totalSold: number;
+
+
+    // Instance attributes
+    title: string;
+    developer: string;
+    availableCopies: number;
+    price: number;
+    copiesSold: number;
+
+    constructor(title: string, developer: string, availableCopies: number, price: number, copiesSold: number, availableGames: string[]) {
+        this.title = title;
+        this.developer = developer;
+        this.availableCopies = availableCopies;
+        this.price = price;
+        this.copiesSold = copiesSold;
+
+        VideoGame.availableGames.push(this);
+    }
+
+    // Instance methods
+    isAvailable(): boolean {
+    return this.availableCopies > 0
+    }
+
+    sell(numberOfCopies:number = 1): number {
+
+        try {
+            if ( this.isAvailable() ) {
+                this.availableCopies -= numberOfCopies;
+                this.copiesSold += numberOfCopies;
+                VideoGame.totalSold = VideoGame.totalSold + this.copiesSold;
+
+                if ( this.availableCopies === 0 ) {
+                    VideoGame.soldOutGames.push(this);
+                    VideoGame.availableGames = VideoGame.availableGames.filter(game => game != this);
+                }
+            }
+
+            return this.price;
+
+        } catch (error) {
+
+            console.error("Not available copies for this game:", error.message);
+            return this.price;
+
+            }
+
+    }
+
+    static getTotalSold(): number {
+        return VideoGame.totalSold;
+    }
+
+    static sellGames(games: VideoGame[], copies?: number[]): number {
+        let totalPrice = 0;
+
+        try {
+            for (let i = 0; i < games.length; i++) {
+                const game = games[i];
+                const numberOfCopies = copies && copies[i] ? copies[i] : 1; // Default to 1 if copies is not provided
+                totalPrice += game.sell(numberOfCopies);
+            }
+
+            return totalPrice;
+        } catch (error) {
+            console.error("Game is not available.", error.message);
+            return totalPrice;
+        } finally {
+        console.log("Game processed successfully");
+        }
+    }
+}
