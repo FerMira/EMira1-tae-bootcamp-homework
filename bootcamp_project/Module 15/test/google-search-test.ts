8e7881640957910d33670cba66b4d1383cf9a470
Fernando Mira <38192049+FerMira@users.noreply.github.com>
2025-06-28 17:31:31 -0300

    Adding meta preserveUrl to avoid CAPTCHA

--- bootcamp_project/Module 15/test/google-search-test.ts
+++ bootcamp_project/Module 15/test/google-search-test.ts
@@ -6,7 +6,9 @@ fixture('Google Search')
 .page('https://www.google.com/');
 
 
-test('User can search something in Google', async t=> {
+test
+.meta({ preserveUrl: true })
+    ('User can search something in Google', async t=> {
 
     const resultPage = new ResultPage(t);
     const searchPage = new SearchPage(t);
