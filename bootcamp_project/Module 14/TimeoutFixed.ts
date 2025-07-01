1e033b3710a70a8e3df0fe8f6fe010c1071c19eb
Fernando Mira <38192049+FerMira@users.noreply.github.com>
2025-07-01 01:23:53 -0300

    Modified as suggested.
    Co-authored-by: Andrés Felipe Aguilar Rendón <afaguilarr96@gmail.com>

--- bootcamp_project/Module 14/TimeoutFixed.ts
+++ bootcamp_project/Module 14/TimeoutFixed.ts
@@ -3,7 +3,7 @@ import { Selector } from 'testcafe';
 fixture('Manual Timeout fixed')
     .page('https://the-internet.herokuapp.com/dynamic_loading/1');
 
-test('Adding custom timeout before clicking', async t => {
+test('Adding custom timeout to "finish" text assertion', async t => {
     await t.click(Selector('button'));
 
     const loadedText = Selector('#finish h4');
